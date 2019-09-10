20909736ff1cc3a17fd66a8d939b8837614962d0
Rifat <rifat.aiub@hotmail.com>
2019-09-10 10:06:08 +0600

    component add

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  title = 'Product';
+  imageUrl = 'https://via.placeholder.com/150/0000FF/808080%20?Text=Digital.com';
   constructor() { }
 
   ngOnInit() {
